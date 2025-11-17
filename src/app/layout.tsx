de8bb84b6ce156b4b83cfcaec3fc6a7e527bc177
ayush tiwari <tiwariayush77@gmail.com>
2025-11-17 22:18:17 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning={true}>
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
         <Toaster />
